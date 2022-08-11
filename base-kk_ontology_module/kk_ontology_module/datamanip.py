6a67a09d7f32b3dbd701809589b7c79f699c9e95
Kaixuan Khoo <kk5217@ic.ac.uk>
2022-08-11 17:37:29 +0100

    Refined the regimen, tumour and patient options for to_pandas

--- base-kk_ontology_module/kk_ontology_module/datamanip.py
+++ base-kk_ontology_module/kk_ontology_module/datamanip.py
@@ -1,25 +1,45 @@
 import pandas as pd
 import numpy as np
+from load_onto import load_onto, TEST_ONTO
 
-def to_pandas(df, onto_class, regimen=True):
+def to_pandas(df, onto_class, patientIDcolname="LINKNUMBER", onto=load_onto(TEST_ONTO)):
     """
     Arguments: 
     df = full dataframe (containing all data)
     onto_class = can be regimen or tumour subclass that you would like to return individuals of
-    regimen = set to True if Regimen subclass, set to false if Tumour subclass
+    patientIDcolname = column name of the PatientID (NHS number, Link number, etc.)
+    onto = full ontology
 
     Takes all instances of the ontology regimen class and returns the pandas df.
+    Currently only supports regimen, tumour and patient subclasses.
     """
     patientIDs = []
     for instance in onto_class.instances():
-        if regimen:
-            patientIDs.append(instance.treats[0].belongs_to_patient[0].PatientID[0])
+        if onto_class in onto.Regimen.subclasses():
+            try:
+                patientIDs.append(instance.treats[0].belongs_to_patient[0].PatientID[0])
+            except:
+                print("Error loading class")
+                return
+        elif onto_class in onto.Tumour.subclasses():
+            try:
+                patientIDs.append(instance.belongs_to_patient[0].PatientID[0])
+            except:
+                print("Error loading class")
+                return
+        elif onto_class in onto.Patient.subclasses():
+            try:
+                patientIDs.append(instance.PatientID[0])
+            except:
+                print("Error loading class")
+                return
         else:
-            patientIDs.append(instance.belongs_to_patient[0].PatientID[0])
+            print("Class is not supported. Currently supported subclasses are of type Regimen, Tumour, and Patient.")
+            return
 
     # print(patientIDs)
     # returns the dataframe where patientID is included
-    return df[df['LINKNUMBER'].isin(patientIDs)]
+    return df[df[patientIDcolname].isin(patientIDs)]
 
 
 
