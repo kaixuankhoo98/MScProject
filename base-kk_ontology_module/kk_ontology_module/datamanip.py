dbd0738d50435f436ce04d12c4385e18a3916bd9
Kaixuan Khoo <kk5217@ic.ac.uk>
2022-08-11 11:25:26 +0100

    added compatibility for tumour or regimen subclass

--- base-kk_ontology_module/kk_ontology_module/datamanip.py
+++ base-kk_ontology_module/kk_ontology_module/datamanip.py
@@ -1,19 +1,21 @@
 import pandas as pd
 import numpy as np
-from owlready2 import *
 
-def to_pandas(df, regimen):
+def to_pandas(df, onto_class, regimen=True):
     """
     Arguments: 
     df = full dataframe (containing all data)
-    regimen = the regimen subclass that you would like to return individuals of
+    onto_class = can be regimen or tumour subclass that you would like to return individuals of
+    regimen = set to True if Regimen subclass, set to false if Tumour subclass
 
     Takes all instances of the ontology regimen class and returns the pandas df.
-    TODO: Make this function suitable for regimens
     """
     patientIDs = []
-    for instance in regimen.instances():
-        patientIDs.append(instance.treats[0].belongs_to_patient[0].PatientID[0])
+    for instance in onto_class.instances():
+        if regimen:
+            patientIDs.append(instance.treats[0].belongs_to_patient[0].PatientID[0])
+        else:
+            patientIDs.append(instance.belongs_to_patient[0].PatientID[0])
 
     # print(patientIDs)
     # returns the dataframe where patientID is included
