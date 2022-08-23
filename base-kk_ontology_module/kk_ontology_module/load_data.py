db3b9d3148d78a1801f293a7a75f5f57773bcbab
Kai <kaixuan.khoo.98@gmail.com>
2022-08-23 15:54:55 +0100

    Added some error handling and more functionality to map_data.py

--- base-kk_ontology_module/kk_ontology_module/load_data.py
+++ base-kk_ontology_module/kk_ontology_module/load_data.py
@@ -7,9 +7,18 @@ def load_data(filename=0, mapped_regimen='MAPPED_REGIMEN'):
     stream = pkg_resources.resource_stream(__name__, TEST_DATA)
     if filename:
         # If the user wants to upload their own ontology
-        df = pd.read_csv(filename) 
+        try:
+            df = pd.read_csv(filename)
+        except(FileNotFoundError):
+            print("FileNotFoundError: Please double check the path and name of your data file.")
+            return
     else:
         # Using the default ontology shipped with the package
-        df = pd.read_csv(stream)
+        try:
+            df = pd.read_csv(stream)
+        except(FileNotFoundError):
+            print("Error loading the test_data file shipped with this package.")
+            return
+
     df = df.astype({mapped_regimen: str})
     return df
